338097badad04202e183eefa31fc8b3e0f2dfca1
Reynold Harbin <reynold@harbin.io>
2017-05-24 11:21:07 -0400

    name change

--- dofindme-config.js
+++ dofindme-config.js
@@ -1,10 +1,10 @@
-//  Created by Reynold Harbin on 5/5/17.
-//
+// created by Reynold Harbin on 5/24/17
+// reynold@digitalocean.com
 
 var logTag = "dofindme-config.js:";
 
 exports.apikeys  = {
- 	prodAPIUrl: "http://api.dofind.me.io:1337/", //PRODUCTION
+ 	prodAPIUrl: "http://api.dofind.me:1337/", //PRODUCTION
  	devAPIUrl: "http://dev-api.dofind.me:1337",  //DEVELOPEMENT
     AppId: "doFindMeAppId-XLut2v5a5z",
 	ClientKey: "doFindMeMasterKey-ITlO9YRqCH",
