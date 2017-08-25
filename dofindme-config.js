f8b9c3b54edb419ff453b1d52048043daba1d410
Reynold Harbin <reynold@digitalocean.com>
2017-08-25 16:02:51 -0700

    removed stale access key

--- dofindme-config.js
+++ dofindme-config.js
@@ -8,7 +8,7 @@ exports.apikeys  = {
  	devAPIUrl: "http://api.devquest.io:3000",  //DEVELOPEMENT
     AppId: "devQuestAppKey-XLut2v5a5z",
 	ClientKey: "devQuestClientAppKey-ITlO9YRqCH",
-	github: "0f2c67e677b5ec7dc4bfe3bbaf4735a27ad351c8"
+	github: "githubAccessToken-addHere"
 }
 
 exports.build  = {
